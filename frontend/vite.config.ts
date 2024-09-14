762514736c15a56966cd7613b3f4c58433195afd
waihsu <hsuw647@gmail.com>
2024-09-14 12:58:08 +0630

    fix vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,11 +11,11 @@ export default defineConfig({
     },
   },
   server: {
-    // proxy: {
-    //   "/api": {
-    //     target: "http://localhost:3000",
-    //     changeOrigin: true,
-    //   },
-    // },
+    proxy: {
+      "/api": {
+        target: "https://hono-app-smbk.onrender.com",
+        changeOrigin: true,
+      },
+    },
   },
 });
